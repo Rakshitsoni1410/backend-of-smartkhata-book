9688f405818094649ca548ebf96de737e9043a20
Rakshitson1410 <rakshitrsoni@gmail.com>
2026-05-27 14:44:02 +0530

    Refactor registerUser function for improved readability and consistency

--- controllers/userController.js
+++ controllers/userController.js
@@ -4,13 +4,9 @@ import jwt from "jsonwebtoken";
 import crypto from "crypto";
 import sendEmail from "../utils/sendEmail.js";
 
-
-
 // ───────────────── REGISTER ─────────────────
 export const registerUser = async (req, res) => {
-
   try {
-
     const {
       name,
       phone,
@@ -22,101 +18,66 @@ export const registerUser = async (req, res) => {
       password,
     } = req.body;
 
-
     // REQUIRED FIELDS
-    if (
-      !name ||
-      !phone ||
-      !email ||
-      !role ||
-      !address ||
-      !password
-    ) {
+    if (!name || !phone || !email || !role || !address || !password) {
       return res.status(400).json({
         success: false,
         message: "Please fill all required fields",
       });
     }
 
-
     // ROLE VALIDATION
     if (role !== "Customer") {
-
       if (!shopName || !businessType) {
-
         return res.status(400).json({
           success: false,
-          message:
-            "Shop name and business type are required",
+          message: "Shop name and business type are required",
         });
-
       }
     }
-
 
     // EMAIL VALIDATION
     const emailRegex = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
 
     if (!emailRegex.test(email)) {
-
       return res.status(400).json({
         success: false,
         message: "Invalid email",
       });
-
     }
-
 
     // PHONE VALIDATION
     const phoneRegex = /^[6-9]\d{9}$/;
 
     if (!phoneRegex.test(phone)) {
-
       return res.status(400).json({
         success: false,
         message: "Invalid phone number",
       });
-
     }
-
 
     // PASSWORD VALIDATION
     if (password.length < 6) {
-
       return res.status(400).json({
         success: false,
-        message:
-          "Password must be at least 6 characters",
+        message: "Password must be at least 6 characters",
       });
-
     }
-
 
     // CHECK USER
     const userExist = await userModel.findOne({
-      $or: [
-        { phone },
-        { email: email.toLowerCase() },
-      ],
+      $or: [{ phone }, { email: email.toLowerCase() }],
     });
 
-
     if (userExist) {
-
       return res.status(409).json({
         success: false,
         message: "User already exists",
       });
-
     }
 
-
     // HASH PASSWORD
-    const hashedPassword = await bcrypt.hash(
-      password,
-      10
-    );
-
+    const hashedPassword = await bcrypt.hash(password, 10);
 
     // CREATE USER
     const user = await userModel.create({
@@ -124,16 +85,11 @@ export const registerUser = async (req, res) => {
       phone,
       email: email.toLowerCase(),
       role,
-      shopName:
-        role === "Customer" ? "" : shopName,
-      businessType:
-        role === "Customer"
-          ? ""
-          : businessType,
+      shopName: role === "Customer" ? "" : shopName,
+      businessType: role === "Customer" ? "" : businessType,
       address,
       password: hashedPassword,
     });
-
 
     // JWT TOKEN
     const token = jwt.sign(
@@ -144,13 +100,12 @@ export const registerUser = async (req, res) => {
       process.env.JWT_SECRET,
       {
         expiresIn: "7d",
-      }
+      },
     );
-
 
     // SEND WELCOME EMAIL
     await sendEmail({
-      to: email,//email should be email but for testing purpose i am using my email
+      to: email, //email should be email but for testing purpose i am using my email
 
       subject: "Welcome to Smart Khata 🎉",
 
@@ -171,7 +126,6 @@ export const registerUser = async (req, res) => {
       `,
     });
 
-
     // RESPONSE
     res.status(201).json({
       success: true,
@@ -184,24 +138,19 @@ export const registerUser = async (req, res) => {
         role: user.role,
       },
     });
-
   } catch (error) {
-
     console.log(error);
 
     res.status(500).json({
       success: false,
       message: "Server error",
     });
-
   }
 };
-
 
 // ───────────────── LOGIN ─────────────────
 export const loginUser = async (req, res) => {
   try {
-
     const { phone, password } = req.body;
 
     if (!phone || !password) {
@@ -238,7 +187,7 @@ export const loginUser = async (req, res) => {
         role: user.role,
       },
       process.env.JWT_SECRET,
-      { expiresIn: "7d" }
+      { expiresIn: "7d" },
     );
 
     res.status(200).json({
@@ -252,71 +201,46 @@ export const loginUser = async (req, res) => {
         phone: user.phone,
       },
     });
-
-  }  catch (error) {
-
-    console.log(
-      "LOGIN ERROR:",
-      error
-    );
+  } catch (error) {
+    console.log("LOGIN ERROR:", error);
 
     res.status(500).json({
-
       success: false,
 
-      message:
-        error.message ||
-        "Server error",
+      message: error.message || "Server error",
     });
-  } s
+  }
 };
-
 
 // ───────────────── FORGOT PASSWORD ─────────────────//
 
 export const forgotPassword = async (req, res) => {
-
   try {
-
     const { email } = req.body;
-
 
     const user = await userModel.findOne({
       email: email.toLowerCase(),
     });
 
-
     // SECURITY
     if (!user) {
-
       return res.json({
         success: true,
-        message:
-          "If email exists, reset link sent",
+        message: "If email exists, reset link sent",
       });
-
     }
 
-
     // GENERATE TOKEN
-    const token = crypto
-      .randomBytes(32)
-      .toString("hex");
-
+    const token = crypto.randomBytes(32).toString("hex");
 
     user.resetPasswordToken = token;
 
-    user.resetPasswordExpires =
-      Date.now() + 15 * 60 * 1000;
-
+    user.resetPasswordExpires = Date.now() + 15 * 60 * 1000;
 
     await user.save();
 
-
     // RESET LINK
-    const resetLink =
-      `${process.env.CLIENT_URL}/reset-password/${token}`;
-
+    const resetLink = `${process.env.CLIENT_URL}/reset-password/${token}`;
 
     // SEND EMAIL
     await sendEmail({
@@ -325,63 +249,60 @@ export const forgotPassword = async (req, res) => {
       subject: "Reset Password",
 
       html: `
-        <div style="font-family:sans-serif">
+<div style="font-family:sans-serif">
 
-          <h2>Password Reset</h2>
+  <h2>Password Reset</h2>
 
-          <p>
-            Click below link to reset password:
-          </p>
+  <p>
+    Click below button to reset password:
+  </p>
 
-          <a href="${resetLink}">
-            Reset Password
-          </a>
+  <a
+    href="${resetLink}"
+    style="
+      display:inline-block;
+      padding:12px 20px;
+      background:#4f46e5;
+      color:white;
+      text-decoration:none;
+      border-radius:8px;
+    "
+  >
+    Reset Password
+  </a>
 
-        </div>
-      `,
+</div>
+`,
     });
-
 
     res.json({
       success: true,
       message: "Reset link sent",
     });
-
   } catch (error) {
-
     console.log(error);
 
     res.status(500).json({
       success: false,
       message: "Server error",
     });
-
   }
 };
 
-
-
 // ───────────────── RESET PASSWORD ─────────────────
 export const resetPassword = async (req, res) => {
-
   try {
-
     const { token } = req.params;
 
     const { password } = req.body;
 
-
     // PASSWORD VALIDATION
     if (password.length < 6) {
-
       return res.status(400).json({
         success: false,
-        message:
-          "Password must be at least 6 characters",
+        message: "Password must be at least 6 characters",
       });
-
     }
-
 
     // FIND USER
     const user = await userModel.findOne({
@@ -392,56 +313,40 @@ export const resetPassword = async (req, res) => {
       },
     });
 
-
     if (!user) {
-
       return res.status(400).json({
         success: false,
-        message:
-          "Token invalid or expired",
+        message: "Token invalid or expired",
       });
-
     }
 
-
     // HASH PASSWORD
-    user.password = await bcrypt.hash(
-      password,
-      10
-    );
-
+    user.password = await bcrypt.hash(password, 10);
 
     // CLEAR TOKEN
     user.resetPasswordToken = undefined;
 
     user.resetPasswordExpires = undefined;
 
-
     await user.save();
-
 
     res.json({
       success: true,
-      message:
-        "Password reset successful",
+      message: "Password reset successful",
     });
-
   } catch (error) {
-
     console.log(error);
 
     res.status(500).json({
       success: false,
       message: "Server error",
     });
-
   }
 };
 
 // ───────────────── GET WHOLESALERS ─────────────────
 export const getWholesalersByBusiness = async (req, res) => {
   try {
-
     const { businessType } = req.params;
 
     const wholesalers = await userModel
@@ -455,7 +360,6 @@ export const getWholesalersByBusiness = async (req, res) => {
       success: true,
       users: wholesalers,
     });
-
   } catch (error) {
     res.status(500).json({
       success: false,

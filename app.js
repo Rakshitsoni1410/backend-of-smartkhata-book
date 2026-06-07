d0ad70e6c32ea553dd99bf9f3b81041daa65d37f
Rakshitson1410 <rakshitrsoni@gmail.com>
2026-06-07 10:37:44 +0530

    Update CORS configuration to allow PATCH method in app.js

--- app.js
+++ app.js
@@ -22,7 +22,7 @@ connectCloudinary();
 
 app.use((req, res, next) => {
   res.setHeader("Access-Control-Allow-Origin", "https://smartkhatabooks.netlify.app");
-  res.setHeader("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE, OPTIONS");
+  res.setHeader("Access-Control-Allow-Methods", "GET, POST, PUT, PATCH, DELETE, OPTIONS");
   res.setHeader("Access-Control-Allow-Headers", "Content-Type, Authorization");
   res.setHeader("Access-Control-Allow-Credentials", "true");
   if (req.method === "OPTIONS") return res.status(200).end();
